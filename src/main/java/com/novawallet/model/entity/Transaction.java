606409b0c30a751304e044360a3c022a7400e821
sara-rioseco <sara.rioseco@gmail.com>
2024-05-10 23:53:01 -0400

    - add Transaction entity getters and setters

--- src/main/java/com/novawallet/model/entity/Transaction.java
+++ src/main/java/com/novawallet/model/entity/Transaction.java
@@ -14,7 +14,7 @@ public class Transaction {
     private int senderAccountId;
     private int receiverUserId;
     private int receiverAccountId;
-    private Timestamp creationDate;
+    private final Timestamp creationDate;
 
     public Transaction(BigDecimal amount, int currencyId, TransactionType transactionType,
                        int senderUserId, int senderAccountId, int receiverUserId,
@@ -27,5 +27,73 @@ public class Transaction {
         this.receiverUserId = receiverUserId;
         this.receiverAccountId = receiverAccountId;
         this.creationDate = Timestamp.from(Instant.from(LocalDateTime.now()));
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public int getCurrencyId() {
+        return currencyId;
+    }
+
+    public void setCurrencyId(int currencyId) {
+        this.currencyId = currencyId;
+    }
+
+    public TransactionType getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(TransactionType transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public int getSenderUserId() {
+        return senderUserId;
+    }
+
+    public void setSenderUserId(int senderUserId) {
+        this.senderUserId = senderUserId;
+    }
+
+    public int getSenderAccountId() {
+        return senderAccountId;
+    }
+
+    public void setSenderAccountId(int senderAccountId) {
+        this.senderAccountId = senderAccountId;
+    }
+
+    public int getReceiverAccountId() {
+        return receiverAccountId;
+    }
+
+    public void setReceiverAccountId(int receiverAccountId) {
+        this.receiverAccountId = receiverAccountId;
+    }
+
+    public int getReceiverUserId() {
+        return receiverUserId;
+    }
+
+    public void setReceiverUserId(int receiverUserId) {
+        this.receiverUserId = receiverUserId;
+    }
+
+    public Timestamp getCreationDate() {
+        return creationDate;
     }
 }
